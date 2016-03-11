8edb7610801a433bd452dc4f9027a066ff113638
Nicole Rasquinha <nrasquinha@berkeley.edu>
2016-03-10 21:57:38 -0800

    Have views and segues. Label and correct button works

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet var guess: UITextField!
+    @IBOutlet var puzzle: UILabel!
+    var puzzleString: String = String()
+    var phrase: String = String()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        phrase = hangmanPhrases.getRandomPhrase()!
         print(phrase)
+        var count = 0
+        for c in phrase.characters {
+            if c == " " {
+                puzzleString.appendContentsOf(" ")
+            }
+            else {
+                puzzleString.appendContentsOf("-")
+            }
+            count += 1
+        }
+        puzzle.text = puzzleString
     }
 
+    @IBAction func correctGuess(sender: UIButton) {
+        let g: Character = guess.text![guess.text!.startIndex]
+        for i in phrase.characters.indices {
+            if phrase.uppercaseString[i] == g || phrase.lowercaseString[i] == g{
+                puzzleString.removeAtIndex(i)
+                puzzleString.insert(phrase[i], atIndex: i)
+            }
+        }
+        puzzle.text = puzzleString
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
